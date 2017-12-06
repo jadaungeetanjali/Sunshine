1774a92591ccf4c01ad500694fe9ffeaf7d3e81c
jadaungeetanjali <jadaungeetanjali@gmail.com>
2017-12-07 01:11:19 +0530

    adapter is attached to populate fake data

--- app/src/main/java/com/silive/pc/weatherapp/MainActivity.java
+++ app/src/main/java/com/silive/pc/weatherapp/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ForecastListFragment forecastListFragment = new ForecastListFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.main_activity, forecastListFragment).commit();
+
     }
 }
