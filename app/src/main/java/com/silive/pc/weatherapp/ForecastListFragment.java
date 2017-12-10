9fffd1cc1b4bdf5f5b9a1a92124f35a9728b32d3
jadaungeetanjali <jadaungeetanjali@gmail.com>
2017-12-10 13:24:51 +0530

    url connection setup in asyncTask and refresh menu_item created

--- app/src/main/java/com/silive/pc/weatherapp/ForecastListFragment.java
+++ app/src/main/java/com/silive/pc/weatherapp/ForecastListFragment.java
@@ -1,14 +1,25 @@
 package com.silive.pc.weatherapp;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -22,12 +33,21 @@ public class ForecastListFragment extends Fragment {
     public ForecastListFragment() {
         // Required empty public constructor
     }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            setHasOptionsMenu(true);
+        }
+    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         View rootView = inflater.inflate(R.layout.fragment_forecast_list, container, false);
         ArrayAdapter<String> mForecastAdapter;
 
@@ -45,7 +65,82 @@ public class ForecastListFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.forecast_list);
         listView.setAdapter(mForecastAdapter);
+
+
         return rootView;
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_item, menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                FetchWeatherTask weatherTask = new FetchWeatherTask();
+                weatherTask.execute();
+                return true;
+            case R.id.settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    public class FetchWeatherTask extends AsyncTask<Void,Void, Void>{
+
+        //private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // will contain the raw jsonResponse as string
+            String forecastJsonStr = null;
+
+            try {
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=26915d6f427789293f314f5d99c12afb");
+                // create the request to openWeatherApp and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                //read the inputStream into string
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                if (inputStream == null){
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line + "\n");
+                }
+                if (buffer.length() == 0){
+                    return null;
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if (urlConnection != null){
+                    urlConnection.disconnect();
+                }
+                if (reader != null){
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
     }
 
 }
